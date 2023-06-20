426a1d1082dc1d8156d74df3a6786ee7bda27c2a
RedConcrete <franzbrunohubertklose@gmail.com>
2023-06-20 17:18:15 +0200

    near finished

--- script.js
+++ script.js
@@ -5,32 +5,52 @@ var tipAmountPerPerson = 0;
 let totalAmount = 0;
 
 let billAmountTextarea;
-let peopleAmountTextarea;
+let peopleAmountTextarea = null;
+let customTextarea;
+
 var billAmount = 0;
 var peopleAmount = 0;
+var customAmount = 0;
 
 var OneProzent = 0;
 
 let spanTip;
 let spanTotal;
 
+let customButton = null;
+
+let isError = false;
+let errorText;
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
+
+    errorText = document.getElementById("error");
     reset();
     checkButtons();
+    checkInputField();
 });
 
 function checkButtons() {
     const btns = document.querySelectorAll('.btn');
+    customTextarea = document.getElementById("7");
 
     btns.forEach(function (button) {
         button.addEventListener('click', function () {
-            if(button.id != 7){
+            if(button.id != 8){
+                if(button.id == 6)
+                {
+                    customButton = button;
+                    button.style.display = 'none';
+                    customTextarea.style.display = 'flex';
+                }
+
                 if (ProcentAmountNumber != button.id) {
                     button.classList.add("btn-activ");
     
                     ProcentAmountNumber = button.id;
+                    console.log (ProcentAmountNumber);
                     calcTip();
     
                     btns.forEach(function (button) {
@@ -50,49 +70,26 @@ function checkButtons() {
     });
 }
 
+function checkInputField(){
 
-function getTipWithout() {
-    switch (ProcentAmountNumber) {
-        case "1": {
-            OneProzent = billAmount / 100;
-            tipAmount = OneProzent * 5;
-            break;
+    customTextarea.addEventListener('focus', function() {
+        customAmount = customTextarea.value;
+        calcTip();
+    });
+
+    customTextarea.addEventListener('input', function() {
+        customAmount = customTextarea.value;
+        console.log("test")
+        calcTip();
+    });
+
+    customTextarea.addEventListener('focusout', function() {
+        console.log(customAmount);
+        if(customAmount == "" ||  customAmount == 0 ||  customAmount == null ){
+            customButton.style.display = 'block';
+            customTextarea.style.display = 'none';
         }
-        case "2": {
-            OneProzent = billAmount / 100;
-            tipAmount = OneProzent * 10;
-            break;
-        }
-
-        case "3": {
-            OneProzent = billAmount / 100;
-            tipAmount = OneProzent * 15;
-            break;
-        }
-
-        case "4": {
-            OneProzent = billAmount / 100;
-            tipAmount = OneProzent * 20;
-            break;
-        }
-
-        case "5": {
-            OneProzent = billAmount / 100;
-            tipAmount = OneProzent * 50;
-            break;
-        }
-
-        case "6": {
-
-
-            break;
-        }
-
-        default: {
-            console.log("Invalid percentage amount.");
-        }
-
-    }
+      });
 }
 
 function getTipWith() {
@@ -131,16 +128,26 @@ function getTipWith() {
             break;
         }
 
-        case "6": {
-
+        case "7": {
+            OneProzent = billAmount / 100;
+            tipAmount = OneProzent * customAmount;
+            tipAmountPerPerson = tipAmount / peopleAmount;
             break;
         }
 
         default: {
             console.log("Invalid percentage amount.");
         }
-
     }
+
+    console.log("ProcentAmountNumber " + ProcentAmountNumber);
+}
+
+function error(){
+
+    errorText.style.display = "block";   
+    
+    
 }
 
 function calcTip() {
@@ -155,13 +162,13 @@ function calcTip() {
         if (peopleAmount != 0) {
             getTipWith();
             spanTip = document.getElementById("tipAmount").innerHTML = "$" + tipAmountPerPerson.toFixed(2);
+            calcTotal();
+            errorText.style.display = "none"
+            console.log("peopleAmount = "+ peopleAmount);
         }
         else {
-            getTipWithout();
-            spanTip = document.getElementById("tipAmount").innerHTML = "$" + tipAmount.toFixed(2);
+            error();
         }
-
-        calcTotal();
     }
     else {
         console.log("No Bill Amount set!!");
@@ -185,12 +192,17 @@ function calcTotal() {
 
 function reset() {
 
-    tipAmount = 0;
-    totalAmount = 0;
+
+    spanTip = document.getElementById("tipAmount").innerHTML = "$" + 0.00;
+    spanTotal = document.getElementById("totalAmount").innerHTML = "$" + 0.00;
+    billAmountTextarea = document.getElementById("billText");
+    peopleAmountTextarea = document.getElementById("poepleText");
+
+    billAmountTextarea.value = 0;
+    peopleAmountTextarea.value = 0;
 
 
-    spanTip = document.getElementById("tipAmount").innerHTML = "$" + tipAmount;
-    spanTotal = document.getElementById("totalAmount").innerHTML = "$" + totalAmount;
+    console.log("reset");
 
 
 }
